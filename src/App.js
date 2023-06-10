5b4d03667e2a70a3fa6bee20af0d597981ac3c29
rbkunwar <baburabin45@gmail.com>
2023-06-11 09:45:43 +1000

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Button } from 'react-bootstrap'
+import { ToastContainer, toast } from 'react-toastify';
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
-  return <div className="">Hello world</div>;
+  toast.success("Rabindra")
+  return (
+ <div className="">
+  <Button variant='danger'>
+  <FaBeer />
+  </Button>
+  Hellow world
+  <ToastContainer />
+  </div>
+  )
 }
 
 export default App;
